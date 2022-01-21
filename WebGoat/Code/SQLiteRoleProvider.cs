79c930b5cd8b7ae4acd39d9ad4eee2b4c48272c9
yuuki1967 <yuki06.endo@gmail.com>
2022-01-21 16:33:00 +0900

    Update SQLiteRoleProvider.cs

--- WebGoat/Code/SQLiteRoleProvider.cs
+++ WebGoat/Code/SQLiteRoleProvider.cs
@@ -5,7 +5,7 @@ using System.Configuration.Provider;
 using System.Data;
 using Mono.Data.Sqlite;
 using System.Web.Security;
-//Comment out Y.Endo #1 #2
+//Comment out Y.Endo #1 #2 #3
 namespace TechInfoSystems.Data.SQLite
 {
 	/// <summary>
